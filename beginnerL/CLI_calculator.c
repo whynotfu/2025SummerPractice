58b7bc00f2666ce4aabb0c25b8b753ccfcda4e09
whynotfu <anadtasyadu.0.o@gmail.com>
2025-06-28 21:34:37 +0300

    a goid version

--- beginnerL/CLI_calculator.c
+++ beginnerL/CLI_calculator.c
@@ -1,59 +1,78 @@
 #include <stdio.h>
 
 float math(int fNum, int sNum, char op){
-    float result; 
+    float result = 0; 
     switch (op)
     {
-    case '+':
+    case '+':{
         result = fNum + sNum;   
         break;
-    case '-':
+    }
+    case '-':{
         result = fNum - sNum;
         break;
-    case '/':
-        result = fNum / sNum;
-        break; 
-    case '*': 
-        result = fNum * sNum;
-        break; 
-    default:
-        printf("Sorry we dont have this :(");
+    }
+    case '/':{
+        if (sNum != 0 ){
+            result = (float)fNum / sNum;
+        }
+        else{
+            printf("\nSorry, you can't divide by 0\n");
+            result = 0;   
+        }
         break;
+    } 
+    case '*':{ 
+        result = fNum * sNum;
+        break;
+    } 
+    default:{
+        printf("Sorry, we don't support this operation :(\n");        
+        break;
+        }
     }
     return result;
 }
 
 void start(){
-    printf("\n1Hello!\n");
-    printf("Welcome to CLI Calculator\n");
-    printf("Choose what do you want to do: \n");
-    printf("1. Use a calculator\n");
-    printf("2. EIXIT\n");
-    int choice;
-    scanf(" %d", &choice);
-    switch (choice)
+    int state = 1;
+    while (state)
     {
-    case 1:
-        int firstNumber, secondNumber;
-        char operator;
-        printf("Entre your first number: \n");
-        scanf("%d", &firstNumber);
-        printf("\n Enter type of an operation (+, -, *, /):\n");
-        scanf(" %c", &operator);
-        printf("\n Enter your seconf number \n");
-        scanf("%d", &secondNumber);
-        printf("\nThe result is:  %f", math(firstNumber,secondNumber,operator));
-        start();
-        break;
-    case 2:
-        printf("\n Bye bye");
-        start();
-        break; 
-    default:
-        printf("\n sorry can't help :(");
+        printf("\nHello!\n");
+        printf("Welcome to CLI Calculator\n");
+        printf("Choose what do you want to do: \n");
+        printf("1. Use a calculator\n");
+        printf("2. EXIT\n");
+        int choice;
+        scanf(" %d", &choice);
+        switch (choice)
+        {
+        case 1: {
+            int firstNumber, secondNumber;
+            char operator;
+            printf("Enter your first number:\n ");
+            scanf("%d", &firstNumber);
 
-        break;
+            printf("\nEnter type of an operation (+, -, *, /):\n");
+            scanf(" %c", &operator);
+
+            printf("\nEnter your second number: \n");
+            scanf("%d", &secondNumber);
+
+            printf("\nThe result is:  %.2f", math(firstNumber,secondNumber,operator));
+            break;
+        }
+        case 2: {
+            printf("\nBye bye");
+            state = 0;
+            break;
+        } 
+        default: {
+            printf("\nSorry can't help :(");
+            break;
+          }  }
     }
+    
 }
 
 int main(){
